21b9836ebc4b3975d2cba7817bf31755526bd6ba
stanl1y <stanleyqaz123@gmail.com>
2022-04-30 16:23:59 +0800

    add highway-env

--- envs.py
+++ envs.py
@@ -1,6 +1,12 @@
 import gym
 from wrapper import *
 def get_env(env_name,wrapper_type):
+    if env_name in ["highway-v0","merge-v0","roundabout-v0","parking-v0","intersection-v0","racetrack-v0"]:
+        '''
+        highway_env:
+        https://github.com/eleurent/highway-env
+        '''
+        import highway_env
     env=gym.make(env_name)
     if wrapper_type=="basic":
         return BasicWrapper(env)
--- example_run_code_sh/train_td3_her.sh
+++ example_run_code_sh/train_td3_her.sh
@@ -1,2 +1,2 @@
-python3 main.py --algo td3 --wrapper_type her --main_stage_type her_off_policy --env FetchReach-v1 --episodes 5000 --use_ounoise
+python3 main.py --algo td3 --wrapper_type her --main_stage_type her_off_policy --env parking-v0 --episodes 10000 --use_ounoise
 #FetchPush-v1
