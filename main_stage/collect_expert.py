b7b3029e71ed63a1ef00b05a689feafde00d237d
stanl1y <stanleyqaz123@gmail.com>
2023-06-02 17:55:20 +0800

    update collect expert

--- main_stage/collect_expert.py
+++ main_stage/collect_expert.py
@@ -15,50 +15,66 @@ class collect_expert:
             )
         self.algo = config.algo
         self.env_id = config.env
+        self.data_name = config.data_name
+        self.weight_path = config.weight_path
+        self.save_env_states = config.save_env_states
 
     def start(self, agent, env, storage):
-        agent.load_weight(self.algo,self.env_id)
+        if self.weight_path:
+            agent.load_weight(path=self.weight_path)
+        else:
+            agent.load_weight(algo=self.algo, env_id=self.env_id)
+        agent.eval()
         if self.based_on_transition_num:
             done = False
-            state = env.reset()
-            total_reward=0
-            episode_reward=0
-            episode_num=0
-            while len(storage) < self.expert_data_num:
+            total_reward = 0
+            episode_reward = 0
+            episode_num = 0
+            state = env.reset(seed=episode_num)
+            while len(storage) < self.expert_transition_num:
                 if done:
-                    state = env.reset()
                     done = False
-                    episode_num+=1
-                    total_reward+=episode_reward
-                    episode_reward=0
+                    episode_num += 1
+                    total_reward += episode_reward
+                    episode_reward = 0
+                    state = env.reset(seed=episode_num)
+                env_state = env.sim.get_state() if self.save_env_states else None
                 action = agent.act(state, testing=True)
                 next_state, reward, done, info = env.step(action)
-                episode_reward+=reward
-                storage.store(state, action, reward, next_state, done)
+                episode_reward += reward
+                storage.store(
+                    state, action, reward, next_state, done, env_state=env_state
+                )
                 state = next_state
-                print(f"expert data recording finish, average reward : {round(total_reward/max(episode_num,1),4)}")
+            avg_reward = round(total_reward / max(episode_num, 1), 4)
+            print(f"expert data recording finish, average reward : {avg_reward}")
             storage.write_storage(
                 self.based_on_transition_num,
-                self.expert_data_num,
+                self.expert_transition_num,
                 self.algo,
                 self.env_id,
+                self.data_name + str(round(avg_reward)),
             )
         else:
-            total_reward=0
-            for _ in range(self.expert_episode_num):
+            total_reward = 0
+            for e in range(self.expert_episode_num):
                 done = False
-                state = env.reset()
+                state = env.reset(seed=e)
                 while not done:
+                    env_state = env.sim.get_state() if self.save_env_states else None
                     action = agent.act(state, testing=True)
                     next_state, reward, done, info = env.step(action)
-                    total_reward+=reward
-                    storage.store(state, action, reward, next_state, done)
+                    total_reward += reward
+                    storage.store(
+                        state, action, reward, next_state, done, env_state=env_state
+                    )
                     state = next_state
+            avg_reward = round(total_reward / self.expert_episode_num, 4)
+            print(f"expert data recording finish, average reward : {avg_reward}")
             storage.write_storage(
                 self.based_on_transition_num,
                 self.expert_episode_num,
                 self.algo,
                 self.env_id,
+                self.data_name + str(round(avg_reward)),
             )
-            print(f"expert data recording finish, average reward : {round(total_reward/self.expert_episode_num,4)}")
-
