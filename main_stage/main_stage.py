9818d15335c2a8a4dab15ab7fb7cad83b393e05b
stanl1y <stanleyqaz123@gmail.com>
2023-06-02 18:01:55 +0800

    update main_stage.py

--- main_stage/main_stage.py
+++ main_stage/main_stage.py
@@ -1,6 +1,7 @@
 from .off_policy import *
 from .collect_expert import collect_expert
-
+from .on_policy import vanilla_on_policy_training_stage
+from .evaluate import evaluate
 
 def get_main_stage(config):
     if config.main_stage_type == "off_policy":
@@ -9,6 +10,10 @@ def get_main_stage(config):
         return her_off_policy_training_stage(config)
     elif config.main_stage_type == "collect_expert":
         return collect_expert(config)
+    elif config.main_stage_type == "on_policy":
+        return vanilla_on_policy_training_stage(config)
+    elif config.main_stage_type == "evaluate":
+        return evaluate(config)
     else:
         raise TypeError(
             f"training stage type : {config.training_stage_type} not supported"
